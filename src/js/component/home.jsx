cb84400372fdd5023a75c6abe1e41852f0b561f1
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-23 11:02:55 +0000

    fix: imports in home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { Jumbotron } from "./Jumbotron";
+import { NavBar } from "./NavBar.jsx";
+import { Jumbotron } from "./Jumbotron.jsx";
+import { Cards } from "./Cards.jsx";
+import { Footer } from "./Footer.jsx";
 
 //create your first component
 const Home = () => {

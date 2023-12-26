a8570df272b055ca6775adfeb6665b99b26e6fee
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-26 18:56:10 +0000

    fix: Añadidos datos placeholder

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import { NavBar } from "./NavBar.jsx";
 import { Jumbotron } from "./Jumbotron.jsx";
 import { Cards } from "./Cards.jsx";
 import { Footer } from "./Footer.jsx";
 
-//create your first component
 const Home = () => {
+	// Codigo para crear el placeholder de las 4 cards
+	let arrayPlaceholder = [{title: 'card-title', subtitle: 'Lorem ipsum', label: 'Find Out More!', image: 'https://placehold.co/500x325'}];
+
+	arrayPlaceholder = Array(4).fill().map(() => arrayPlaceholder[0]);
+	
 	return (
 		<NavBar />
 		<Jumbotron />
-		<Cards />
+		<Cards valores={arrayPlaceholder}/>
 		<Footer />
 	);
 };

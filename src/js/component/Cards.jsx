721016eee44aa32d8b2827124464809d85c3a43b
Irene VS <147383929+iireneVS@users.noreply.github.com>
2023-12-26 19:01:59 +0000

    feat: fin

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-export const Cards = ({title='card-title', subtitle='Lorem ipsum', label='Find Out More!', image='https://placehold.co/500x325'}) => {
-    
-const valores = [1, 2, 3, 4];
+export const Cards = ({valores=[{title : 'card-title', subtitle : 'Lorem ipsum', label : 'Find Out More!', image : 'https://placehold.co/500x325'}]}) => {
 
 return (
+  
     <div className="cards-container" style={{ display: 'flex', flexWrap: 'wrap' }}>
       {valores.map((valor, index) => (
         <div className="card" style={{ width: "18rem", margin: '10px' }} key={index}>
-          <img src={image} className="card-img-top" alt="..." />
+          <img src={valor.image} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{subtitle}</p>
-            <a href="#" className="btn btn-primary">{label}</a>
+            <h5 className="card-title">{valor.title}</h5>
+            <p className="card-text">{valor.subtitle}</p>
+            <a href="#" className="btn btn-primary">{valor.label}</a>
           </div>
         </div>
       ))}

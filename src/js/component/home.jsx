77ff9878c40398d4ab86949cbbef09f7819536af
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-26 20:04:49 +0000

    fix: home.jsx & jumbotron.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,15 +7,17 @@ import { Footer } from "./Footer.jsx";
 
 const Home = () => {
 	// Codigo para crear el placeholder de las 4 cards
-	let arrayPlaceholder = [{title: 'card-title', subtitle: 'Lorem ipsum', label: 'Find Out More!', image: 'https://placehold.co/500x325'}];
+	let arrayPlaceholder = [{ title: 'card-title', subtitle: 'Lorem ipsum', label: 'Find Out More!', image: 'https://placehold.co/500x325' }];
 
 	arrayPlaceholder = Array(4).fill().map(() => arrayPlaceholder[0]);
-	
+
 	return (
-		<NavBar />
-		<Jumbotron />
-		<Cards valores={arrayPlaceholder}/>
-		<Footer />
+		<>
+			<NavBar />
+			<Jumbotron />
+			<Cards valores={arrayPlaceholder} />
+			<Footer />
+		</>
 	);
 };
 

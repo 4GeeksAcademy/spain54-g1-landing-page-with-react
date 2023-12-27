bffb128673532329ec45de08d46a59c3c355fe1a
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-27 15:08:39 +0000

    Fix: bg color & text size

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -4,10 +4,10 @@ import React from "react";
 export const Jumbotron = ({ title = 'A Warm Welcome!', subtitle = 'Lorem Ipsum', label = 'Call to action!' }) => {
     return (
         <div className="container">
-            <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+            <div className="p-5 mb-4 bg-light rounded-3">
                 <div className="container-fluid py-5">
                     <h1 className="display-5 fw-bold">{title}</h1>
-                    <p className="col-md-8 fs-4">{subtitle}</p>
+                    <p className="col-md-8 fs-5">{subtitle}</p>
                     <button className="btn btn-primary btn-lg" type="button">{label}</button>
                 </div>
             </div>
